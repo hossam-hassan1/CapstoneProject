699a29ce7a9badb3aa49a86ff0595510b4801f5f
Kenna <kennabogue@gmail.com>
2022-10-14 22:17:59 -0500

    added queries to database

--- database/scavyQueries.py
+++ database/scavyQueries.py
@@ -14,6 +14,7 @@ def create_query(query, success):
         )
         mycursor = mydb.cursor()
         mycursor.execute(query)
+        mydb.commit()
         print(f"{success}")
     except Exception as err:
         print(f"Error Occured: {err}\nExiting program...")
@@ -36,7 +37,7 @@ def search_query(query):
         quit()
 
 
-#     -- create_user() - an insert query into Users Table  (email, user_name, password)
+# create_user() - an insert query into Users Table  (email, user_name, password)
 def create_user(email, username, password):
     insert = f"""
         INSERT INTO Users (email, username, password)
@@ -44,31 +45,46 @@ def create_user(email, username, password):
     success = "User added!"
     create_query(insert, success)
     
-create_user("testemail", "bob", "pass")
+
 # -- login.html (Users Table)
 
-def get_user():
-    select = """
-    SELECT * FROM Users;
+# get_user() - a get query to validate user exists and return user account info 
+#  (associated games created) (email, user_name, password)
+def get_user(username, password):
+    select_user = f"""
+    SELECT user_id FROM Users WHERE username = "{username}" AND password = "{password}";
     """
-    result = search_query(select)
-    
+    result = search_query(select_user)
+    if len(result) == 1:
+        select_games = f"""
+        SELECT game_title FROM Games WHERE user_id = 1;
+        """
+        result = search_query(select_games)
+        print(f"{username}'s Games:")
+        print(result)
+    else:
+        print("user does not exit")
 
-get_user()
-
-#     -- get_user() - a get query to validate user exists and return user account info 
-#         -- (associated games created) (email, user_name, password)
-#     SELECT * FROM Users 
-#     WHERE user_id = "user_id" AND password = "password";
+get_user('test', 'test')
 
 
-# -- create_game.html
+# create_game.html
 
-#     -- create_game() - an insert query into Games, Clues, and Locations Tables
+#  create_game() - an insert query into Games, Clues, and Locations Tables
 
+def create_game(user_id, game_title, game_description, privacy_level, gps_required, camera_required):
+    insert = f"""
+    INSERT INTO Games (user_id, game_title, game_description, privacy_level, gps_required, camera_required)
+    VALUES ({user_id}, "{game_title}", "{game_description}", "{privacy_level}", "{gps_required}", "{camera_required}");
+    """
+    success = "Game created!"
+    create_query(insert, success)
+
+# create_game(1, "test title", "test description", "public", "true", "false")
 
 
 # -- play/search.html
+
 
 #     -- get_game() - a get query to find game by game code from Games Tables
 
