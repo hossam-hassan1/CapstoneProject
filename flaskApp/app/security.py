21364528ac6f21ddea93d4f2cdbfaddbe70c552f
Kenna <kennabogue@gmail.com>
2022-12-09 04:17:31 -0600

    Production Branch Files

---
+++ flaskApp/app/security.py
@@ -0,0 +1,108 @@
+# import rsa
+
+# # HASH PASSWORDS
+# # -------------------------
+
+# # do = True
+# # while(do == True):
+# #     help = hash(input())
+# #     hello = hash(input())
+# #     if help == hello:
+# #         print("Hashes Match!")
+# #         do = False
+# #     else:
+# #         print("Try Again.")
+    
+# # ENCRYPT LOCATION
+# # -------------------------
+
+# # this is the string that we will be encrypting
+# coordinate = "38.94200875265407, -92.32646834504295"
+# publicKey, privateKey = rsa.newkeys(512)
+
+def separateCoordinates(coordinate):
+    longitude = 0
+    latitude = 0
+    coordinates = x = coordinate.split(", ")
+    if len(coordinates) == 2:
+        latitude = (coordinates[0])
+        longitude = (coordinates[1])
+    return latitude, longitude
+
+# # latitude, longitude = separateCoordinates(coordinate)
+# # print(latitude)
+# # print(longitude)
+
+
+# # reference: https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-strings-in-python/
+# def encryptData(data):
+#     #generate keys
+#     #encode message
+#     encMessage = rsa.encrypt(data.encode(), publicKey)
+#     decMessage = rsa.decrypt(encMessage, privateKey).decode()
+#     print("encrypted string: ", encMessage)
+#     print("decrypted string: ", decMessage)
+#     return encMessage
+
+# encryptData(latitude)
+# print(latitude)
+    
+ 
+def validatePassword(password, confirm_password):
+    upper = False
+    lower = False
+    number = False
+    special = False
+    length = False
+    check = False
+    confirm = False
+    error = []
+    if confirm_password == password:
+        confirm = True
+    else:
+        error.append("Passwords don't match.")
+    if len(password) > 7:
+        length = True
+    for letter in password:
+        if letter.isupper() == True:
+            upper = True
+        elif letter.islower() == True:
+            lower = True
+        elif letter in ["0","1","2","3","4","5","6","7","8","9"]:
+            number = True
+        elif letter in ["!", "$", "#", "@", "%", "&", "-"]:
+            special = True
+        else:
+            error.append(f"'{letter}' is an invalid character.")
+    if upper == lower == number == length == special == confirm == True:
+        check = True
+    else:
+        if len(password) < 8:
+            error.append("Password must contain 8 or more characters.")
+        if upper == False:
+            error.append("Password is missing an upper case letter.")
+        if lower == False:
+            error.append("Password is missing a lower case letter.")
+        if number == False:
+            error.append("Password is missing a number.")
+        if special == False:
+            error.append("Password is missing a special character !, $, #, @, %, &.")
+    message = [line for line in error]
+    return check, message     
+
+
+
+def passwordMessage(password, confirm_password):
+    # password = input("Enter a password: ")
+    validation = validatePassword(password, confirm_password)
+    if validation[0] == True:
+        print("\nThe password is valid")
+    else:
+        print("\nPlease try again. Fix these errors:")
+        for line in validation[1]:
+            print(line)
+
+
+# passwordMessage("Passw0r!df", "Passw0r!df")
+
+
---
+++ flaskApp/app/static/js/placeLibary.js
@@ -0,0 +1,189 @@
+function initAutocomplete() {
+  var mapStartingLocation = {lat:38.85637240807301,lng: -90.31152627553638};
+  var options = {
+    zoom:10,
+    center: mapStartingLocation
+}
+// Create map
+var map = new google.maps.Map(document.getElementById('map'),options)
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+
+  
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+  
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+    
+    
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+  
+  // Allow only one marker
+  g = 0
+  google.maps.event.addListener(map, 'click', function( event ){
+    for(g;g<1;g++){
+      userSelectedMarker = {coords:{lat:event.latLng.lat(), lng:event.latLng.lng()}};
+      markersArray.push(userSelectedMarker);
+      
+    }
+    // Add multiple markers to the Array
+    // for(var i = 0;i<markersArray.length;i++)
+    // {
+    //   addMarker(markersArray[i]);
+    // }
+    
+    
+    console.log(markersArray);
+    // Add one marker to the Array
+    i = 0
+    for(i;i<1;i++)
+    {
+      addMarker(markersArray[i]);
+    }
+  });
+  
+  var markersArray = [];
+
+    // Reload map (remove markers)
+    clearButton.addEventListener('click', () => {
+      console.log("clear Map");
+      
+    })
+ 
+    
+    // Add marker functions
+    function addMarker(props){
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            // Add markers to this map
+            map: map
+        });
+        showPosition(props);    
+    }
+
+    //fetch cords to database
+function showPosition(props) {
+  console.log(props.coords.lat);
+  let latitude = props.coords.lat;
+  let longitude = props.coords.lng;
+  let location = '('+latitude+', '+longitude+')';
+  console.log(location);
+  console.log(JSON.stringify({ location }))
+  const url = {{ request.url|tojson }}
+  let create_url = `${url}`
+  fetch(create_url, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ location })
+  })
+  .then((response)=>{         
+      if(response.redirected){
+          window.location.href = response.url;
+      }
+  })     
+}
+    
+    // Geolocation
+    infoWindow = new google.maps.InfoWindow();
+    
+    // locationButton being made
+    // const locationButton = document.createElement("button");
+
+    locationButton.textContent = "Pan to Current Location";
+    //document.getElementById("locationButton").style.color = "red";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+            const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Location found.");
+            infoWindow.open(map);
+            map.setCenter(pos);
+            },
+            () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+        } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
+}
+
+
+// This function promps the user that location is disabled
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  window.alert("Location service is disabled")
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+      browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  )
+infoWindow.open(map);
+}
+// Function that reloads the map (removes marker)
+// const url = 'http://127.0.0.1:5000/geolocation';
+// URL that helped with the clear map dom error
+function reload(map){
+
+  initAutocomplete();
+  markersArray = [];
+
+ //this line is to watch the result in console , you can remove it later	
+  console.log("Refreshed"); 
+}
+const clearButton = document.createElement("button")
+clearButton.innerHTML = "Clear Map"
+
+// clearButton.appendChild(clearButton);
+var selectClearMap = document.getElementById("clearButtonDiv");
+
+selectClearMap.addEventListener("click", reload);
+// selectClearMap.appendChild(clearButton);
+
